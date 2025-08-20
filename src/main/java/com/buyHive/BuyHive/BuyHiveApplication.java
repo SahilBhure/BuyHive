2d6f9fea61960ab106d1cf99661b6ea901611a99
SahilBhure <79324481+SahilBhure@users.noreply.github.com>
2025-08-20 12:01:02 +0530

    First commit

---
+++ src/main/java/com/buyHive/BuyHive/BuyHiveApplication.java
@@ -0,0 +1,13 @@
+package com.buyHive.BuyHive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyHiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buyHive/BuyHive/BuyHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyHive.BuyHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
