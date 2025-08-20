95fb9241bc6ccc864e1172b14372f9393e8b3a1c
SahilBhure <79324481+SahilBhure@users.noreply.github.com>
2025-08-20 12:41:31 +0530

    Added User Details DB Schema

---
+++ src/main/java/Data/UserDetails.java
@@ -0,0 +1,68 @@
+package Data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserDetails {
+
+    @Id
+    @GeneratedValue
+    private int ID;
+
+    private String Name;
+
+    private String Password;
+
+    private String Address;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public UserDetails(int ID, String name, String password, String address) {
+        this.ID = ID;
+        Name = name;
+        Password = password;
+        Address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDetails{" +
+                "ID=" + ID +
+                ", Name='" + Name + '\'' +
+                ", Password='" + Password + '\'' +
+                ", Address='" + Address + '\'' +
+                '}';
+    }
+}
